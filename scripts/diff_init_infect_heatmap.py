5fb0556e1c21152043faab817f70ced7b816ca0d
Abbie Evans <147088150+abbie-evans@users.noreply.github.com>
2026-04-29 11:09:12 +0100

    Add script to plot heatmap

---
+++ scripts/diff_init_infect_heatmap.py
@@ -0,0 +1,91 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# ---------------------------------------------------------------------------------------
+# Deaths
+
+delay_deaths = []
+for multiplier in [1, 1.25, 1.5, 1.75, 2, 2.25, 2.5]:
+    deaths = pd.read_csv(f'data/re1.5/50/deaths_{multiplier}.csv', header=None, dtype=float)
+    delay_deaths.append(list(deaths.sum(axis=1)))
+
+d = np.array(delay_deaths[::-1])
+
+# Define axis labels
+x_labels = ['2.5', '2.25', '2', '1.75', '1.5', '1.25', '1']
+
+diff = -(d - min(d[-1]))
+final_points = []
+for i in range(len(diff)):
+    for j in range(len(diff[0])):
+        if diff[i][j] >= 0:
+            if all(diff[i][j+k] < 0 for k in range(1, len(diff[0]) - j)):
+                final_points.append(j)
+
+plt.figure(figsize=(8, 6))
+diff = diff[:-1, :]
+final_points = final_points[:-1]
+x_labels = x_labels[:-1]
+ax = sns.heatmap(diff.T, cmap='bwr_r', center=0, vmax=10, vmin=-15)
+# for each row in diff, do a vertical black line at the corresponding final_points value
+for i in range(len(diff)):
+    if i < len(final_points):
+        ax.hlines(y=final_points[i], xmin=i, xmax=i+1, color='black', linewidth=2, linestyle='--')
+ax.invert_yaxis()
+ax.invert_xaxis()
+ax.set_yticks([0+0.5, 30+0.5, 60+0.5, 90+0.5, 120+0.5, 150+0.5, 180+0.5])
+ax.set_yticklabels(np.linspace(0, 180, 7, dtype=int), fontsize=18)
+ax.set_xticklabels(x_labels, fontsize=18)
+colorbar = ax.collections[0].colorbar
+colorbar.ax.tick_params(labelsize=18)
+colorbar.set_label('Number fewer deaths compared to' '\n' 'optimal deployment of existing vaccine', rotation=90, labelpad=10, fontsize=18)
+plt.xticks(rotation=0)
+plt.ylabel('Start of booster vaccination (days)', labelpad=10, fontsize=20)
+plt.xlabel(r'Vaccine efficacy ($\sigma$)', labelpad=10, fontsize=20)
+plt.tight_layout()
+plt.savefig("figures/deaths_init_50.svg", bbox_inches="tight")
+plt.show()
+
+delay_deaths = []
+for multiplier in [1, 1.25, 1.5, 1.75, 2, 2.25, 2.5]:
+    deaths = pd.read_csv(f'data/re1.5/200/deaths_{multiplier}.csv', header=None, dtype=float)
+    delay_deaths.append(list(deaths.sum(axis=1)))
+
+d = np.array(delay_deaths[::-1])
+
+# Define axis labels
+x_labels = ['2.5', '2.25', '2', '1.75', '1.5', '1.25', '1']
+
+diff = -(d - min(d[-1]))
+final_points = []
+for i in range(len(diff)):
+    for j in range(len(diff[0])):
+        if diff[i][j] >= 0:
+            if all(diff[i][j+k] < 0 for k in range(1, len(diff[0]) - j)):
+                final_points.append(j)
+
+plt.figure(figsize=(8, 6))
+diff = diff[:-1, :]
+final_points = final_points[:-1]
+x_labels = x_labels[:-1]
+ax = sns.heatmap(diff.T, cmap='bwr_r', center=0, vmax=10, vmin=-15)
+# for each row in diff, do a vertical black line at the corresponding final_points value
+for i in range(len(diff)):
+    if i < len(final_points):
+        ax.hlines(y=final_points[i], xmin=i, xmax=i+1, color='black', linewidth=2, linestyle='--')
+ax.invert_yaxis()
+ax.invert_xaxis()
+ax.set_yticks([0+0.5, 30+0.5, 60+0.5, 90+0.5, 120+0.5, 150+0.5, 180+0.5])
+ax.set_yticklabels(np.linspace(0, 180, 7, dtype=int), fontsize=18)
+ax.set_xticklabels(x_labels, fontsize=18)
+colorbar = ax.collections[0].colorbar
+colorbar.ax.tick_params(labelsize=18)
+colorbar.set_label('Number fewer deaths compared to' '\n' 'optimal deployment of existing vaccine', rotation=90, labelpad=10, fontsize=18)
+plt.xticks(rotation=0)
+plt.ylabel('Start of booster vaccination (days)', labelpad=10, fontsize=20)
+plt.xlabel(r'Vaccine efficacy ($\sigma$)', labelpad=10, fontsize=20)
+plt.tight_layout()
+plt.savefig("figures/deaths_init_200.svg", bbox_inches="tight")
+plt.show()
